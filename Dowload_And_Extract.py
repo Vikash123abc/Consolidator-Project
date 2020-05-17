905c51a4262d8142aa441d7547a6bc88486fdd88
Vikash123abc <vkumar@mt.iitr.ac.in>
2020-05-17 19:30:32 +0530

    Updating the code for downloading and extracting

---
+++ Dowload_And_Extract.py
@@ -0,0 +1,40 @@
+
+import requests
+
+def download_tar(url,name):
+    print("Downloading Started")
+    r = requests.get(url, allow_redirects=True)
+    print("Successfully Downloaded")
+    open(name, 'wb').write(r.content)
+
+# print("Downloaded_module successfully called")
+
+#Program for extracting all items of a file and putting in a folder called output.
+import tarfile as tf
+import os
+
+def extract_tar(name):
+    print("Extracting Started of {}".format(name))
+    a = tf.open(name)
+    output_name = "./Output/extracted" + os.path.splitext(name)[0]
+    a.extractall(output_name)
+    a.close()
+    print("Extracting Finished")
+
+# print("imported extract_modules_successfully")
+
+
+
+def main():
+
+    download_tar("http://www.shallalist.de/Downloads/shallalist.tar.gz","1.tar")
+    download_tar("http://dsi.ut-capitole.fr/blacklists/download/blacklists.tar.gz","2.tar")
+    download_tar("http://squidguard.mesd.k12.or.us/blacklists.tgz","3.tar")
+    extract_tar("1.tar")
+    extract_tar("2.tar")
+    extract_tar("3.tar")
+
+    
+
+if __name__ == '__main__':
+    main()
